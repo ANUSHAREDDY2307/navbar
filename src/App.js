2e1aa790e8ef954bf8f95c30e4e13c570d80b77d
Monitha <monithaa2001@atlassian.com>
2020-09-12 12:04:42 +0530

    Navbar using react-router

--- src/App.js
+++ src/App.js
@@ -1,55 +1,26 @@
 import React, { Component } from 'react';
-import { FaAlignRight } from 'react-icons/fa';
-import './App.css';
-export default class App extends Component {
-    state = {
-        toggle:false
-    }
-    Toggle = () => {
-        this.setState({toggle:!this.state.toggle})
-    }
-    render() {
-        const li = [
-            {
-                link: "/",
-                text:"Home"
-            },
-            {
-                link: "/about/",
-                text:"About us"
-            },
-            {
-                link: "/contact/",
-                text:"Contact us"
-            },
-            {
-                link: "/our courses/",
-                text:"Our courses"
-            },
-            {
-                link: "/Join our community/",
-                text:"Join our community"
-            },
-            {
-                link: "/Join our discussion forum/",
-                text:"Join our discussion forum"
-            }
- ];
-        return (
-            <>
-            <div className="navBar">
-                  <button onClick={this.Toggle}>
-                      <FaAlignRight />
-                  </button>
-                  <ul className={this.state.toggle ? "links show-nav" : "links"}>
-                      {
-                          li.map((objLink, i) => {
-                            return ( <li key={i}><a href={objLink.link}>{objLink.text}</a></li> )
-                          })
-                      }
-                  </ul>
-            </div>
-        </>
-        );
-    }
+import Navbar from './components/Navbar';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Community from './components/Community';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+      <div className="App">
+        <Navbar/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/contact" component={Contact}/>
+          <Route path="/community" component={Community}/>
+        </Switch>
+      </div>
+      </BrowserRouter>
+    );
+  }
 }
+export default App;
---
+++ src/components/Community.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Community extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Join our Community Page</h1>
+            </div>
+        );
+    }
+}
+export default Community;
